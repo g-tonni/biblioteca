81b2a7c424fe3b8fe34bba7b6d2c9f30d603262d
Giada <tonni.giada.210802@gmail.com>
2026-07-14 12:19:48 +0200

    complete runner and adds first admin

--- src/main/java/giada/tonni/biblioteca/runners/StartRunner.java
+++ src/main/java/giada/tonni/biblioteca/runners/StartRunner.java
@@ -1,20 +1,48 @@
 package giada.tonni.biblioteca.runners;
 
 import giada.tonni.biblioteca.entities.Ruolo;
+import giada.tonni.biblioteca.entities.Utente;
 import giada.tonni.biblioteca.payloads.RuoloDTO;
+import giada.tonni.biblioteca.payloads.UtenteDTO;
 import giada.tonni.biblioteca.services.RuoliService;
+import giada.tonni.biblioteca.services.UtentiService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Component
 public class StartRunner implements CommandLineRunner {
 
     private final RuoliService ruoliService;
+    private final UtentiService utentiService;
+    private final String adminName;
+    private final String adminSurname;
+    private final LocalDate adminBirth;
+    private final String adminEmail;
+    private final String adminPassword;
+    ;
+    private final long adminPhone;
+    ;
 
-    public StartRunner(RuoliService ruoliService) {
+    public StartRunner(RuoliService ruoliService,
+                       UtentiService utentiService,
+                       @Value("${admin.name}") String adminName,
+                       @Value("${admin.surname}") String adminSurname,
+                       @Value("${admin.birth}") LocalDate adminBirth,
+                       @Value("${admin.email}") String adminEmail,
+                       @Value("${admin.password}") String adminPassword,
+                       @Value("${admin.phone}") long adminPhone) {
         this.ruoliService = ruoliService;
+        this.utentiService = utentiService;
+        this.adminName = adminName;
+        this.adminSurname = adminSurname;
+        this.adminBirth = adminBirth;
+        this.adminEmail = adminEmail;
+        this.adminPassword = adminPassword;
+        this.adminPhone = adminPhone;
     }
 
     @Override
@@ -22,12 +50,22 @@ public class StartRunner implements CommandLineRunner {
 
         List<Ruolo> listaRuoli = this.ruoliService.findAllRuoli();
         if (listaRuoli.isEmpty()) {
-            RuoloDTO admin = new RuoloDTO("ADMIN");
-            RuoloDTO utente = new RuoloDTO("UTENTE");
+            RuoloDTO admin = new RuoloDTO("ROLE_ADMIN");
+            RuoloDTO utente = new RuoloDTO("ROLE_UTENTE");
 
             this.ruoliService.saveRuolo(admin);
             this.ruoliService.saveRuolo(utente);
         }
 
+        List<Utente> listaUtenti = this.utentiService.findAllUtenti();
+
+        if (listaUtenti.isEmpty()) {
+            UtenteDTO adminBody = new UtenteDTO(adminName, adminSurname, adminBirth, adminEmail, adminPassword, adminPhone);
+            this.utentiService.addFirstAdmin(adminBody);
+        }
+
+
     }
+
+
 }
